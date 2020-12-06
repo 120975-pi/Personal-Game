bb7422380e1af5e63b0046ebbd0742d83470a36b
120975-pi <70445943+120975-pi@users.noreply.github.com>
2020-12-06 12:10:37 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var distance = 0;
+var database;
+
+var player, ground
+
+var playerImage, groundImage
+
+function preload(){
+  playerImage = loadImage("Player_Character_Level_1-removebg-preview");
+  groundImage = loadImage("Level_1_ground-removebg-preview");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  
+}
+
+
+function draw(){
+ 
+  
+}
